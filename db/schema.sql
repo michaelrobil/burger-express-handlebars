b2b793eb9fad28226de3b05f2898e2e0087d6c70
Michael Robil <ch.michaelsamir@hotmail.com>
2019-12-16 10:04:09 -0600

    new files added

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_DB;
+USE burgers_DB;
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
