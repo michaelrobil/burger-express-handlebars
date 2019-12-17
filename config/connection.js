f820a04b960a6a2993cec04771246de88b772c09
Michael Robil <ch.michaelsamir@hotmail.com>
2019-12-16 21:52:38 -0600

    connection updated

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Mikololo1@",
-    database: "burgers_DB"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Mikololo1@",
+        database: "burgers_DB"
+    });
+}
 connection.connect(err => {
     if (err) {
         console.error("error connecting: " + err.stack);
